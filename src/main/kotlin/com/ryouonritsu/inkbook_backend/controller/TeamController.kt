60cb6fb79171d27405f925a2182f81a1b9c0f9cd
kude77 <412724618@qq.com>
2022-08-01 18:01:30 +0800

    test

--- src/main/kotlin/com/ryouonritsu/inkbook_backend/controller/TeamController.kt
+++ src/main/kotlin/com/ryouonritsu/inkbook_backend/controller/TeamController.kt
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
 
 /**
  *
@@ -15,8 +16,15 @@ class TeamController {
     @Autowired
     lateinit var teamService: TeamService
 
-    @PostMapping("/group/create")
-    fun createNewTeam(@RequestBody body: Map<String, String>): Map<String, Any> {
+    @PostMapping("/team/create")
+    fun createNewTeam(@RequestBody body: Map<String, String>, request: HttpServletRequest): Map<String, Any> {
+        var userId = request.session.getAttribute("userId").toString().let {
+            if (it.isNullOrBlank()) return mapOf(
+                "success" to false,
+                "message" to "用户未登录！"
+            )
+            else it
+        }
         return mapOf(
             "success" to true,
             "message" to "todo"
