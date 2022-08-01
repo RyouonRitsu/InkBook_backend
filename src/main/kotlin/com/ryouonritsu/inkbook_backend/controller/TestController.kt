a0f1d59e7349e2992b17d1b764ef41798cd3c88e
kude77 <412724618@qq.com>
2022-08-01 20:41:45 +0800

    取消@CrossOrigin跨域

--- src/main/kotlin/com/ryouonritsu/inkbook_backend/controller/TestController.kt
+++ src/main/kotlin/com/ryouonritsu/inkbook_backend/controller/TestController.kt
@@ -1,12 +1,10 @@
 package com.ryouonritsu.inkbook_backend.controller
 
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@CrossOrigin
 class TestController {
     @PostMapping("/test")
     fun test(@RequestBody body: Map<String, String>): Map<String, Any> {
