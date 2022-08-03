2359a77e61a4df361ac76ee51be1e4d3efc18059
橙原 凯 <1780645196@qq.com>
2022-08-03 11:45:25 +0800

    拦截器修改为不需要提供user_id

--- src/main/kotlin/com/ryouonritsu/inkbook_backend/config/TokenInterceptor.kt
+++ src/main/kotlin/com/ryouonritsu/inkbook_backend/config/TokenInterceptor.kt
@@ -23,14 +23,15 @@ class TokenInterceptor : HandlerInterceptor {
         }
         response.characterEncoding = "UTF-8"
         val token = request.getParameter("token")
-        val user_id = request.getParameter("user_id")
+        //val user_id = request.getParameter("user_id")
         if (!token.isNullOrBlank()) {
 //            if (TokenUtils.verify(token).first) {
 //                log.info("通过拦截器")
 //                return true
 //            }
+            val (re, user_id) = TokenUtils.verify(token)
             log.info("现有的token: $token")
-            if (redisUtils[user_id] == token) {
+            if (redisUtils[user_id] == token && re) {
                 log.info("通过拦截器")
                 return true
             } else {
