aa99d1133cce1fcb3c9dba5ce42864ebe7c8e759
RyouonRitsu <zhou.hongxi0903@qq.com>
2022-08-05 21:22:52 +0800

    收藏和最近浏览外键删除bug

--- src/main/kotlin/com/ryouonritsu/inkbook_backend/repository/User2DocumentationRepository.kt
+++ src/main/kotlin/com/ryouonritsu/inkbook_backend/repository/User2DocumentationRepository.kt
@@ -4,7 +4,12 @@ import com.ryouonritsu.inkbook_backend.entity.Documentation
 import com.ryouonritsu.inkbook_backend.entity.User
 import com.ryouonritsu.inkbook_backend.entity.User2Documentation
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface User2DocumentationRepository : JpaRepository<User2Documentation, Long> {
     fun findByUserAndDoc(user: User, doc: Documentation): User2Documentation?
+
+    @Query("SELECT u FROM User2Documentation u WHERE u.doc.did = :docId")
+    fun findByDocId(@Param("docId") docId: Long): List<User2Documentation>
 }
