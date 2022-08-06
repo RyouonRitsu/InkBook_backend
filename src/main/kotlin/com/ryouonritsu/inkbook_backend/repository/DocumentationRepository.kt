41104f653f0b62ba40c32ff31905fed1ca35e291
RyouonRitsu <zhou.hongxi0903@qq.com>
2022-08-06 23:17:31 +0800

    收藏和最近浏览外键删除bug

--- src/main/kotlin/com/ryouonritsu/inkbook_backend/repository/DocumentationRepository.kt
+++ src/main/kotlin/com/ryouonritsu/inkbook_backend/repository/DocumentationRepository.kt
@@ -3,7 +3,9 @@ package com.ryouonritsu.inkbook_backend.repository
 import com.ryouonritsu.inkbook_backend.entity.Documentation
 import com.ryouonritsu.inkbook_backend.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface DocumentationRepository : JpaRepository<Documentation, Long> {
     fun findByCreator(creator: User): List<Documentation>
     fun findByPid(pId: Int): List<Documentation>
--- src/main/kotlin/com/ryouonritsu/inkbook_backend/repository/UserRepository.kt
+++ src/main/kotlin/com/ryouonritsu/inkbook_backend/repository/UserRepository.kt
@@ -2,7 +2,9 @@ package com.ryouonritsu.inkbook_backend.repository
 
 import com.ryouonritsu.inkbook_backend.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository : JpaRepository<User, Long> {
     fun findByUsername(username: String): User?
     fun findByEmail(email: String): User?
