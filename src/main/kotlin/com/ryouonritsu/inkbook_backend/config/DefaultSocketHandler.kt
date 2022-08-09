1a00bfdf122181a50a4da36ce169b9cecf8df192
kude77 <412724618@qq.com>
2022-08-09 22:14:47 +0800

    同步计划1.0

--- src/main/kotlin/com/ryouonritsu/inkbook_backend/config/DefaultSocketHandler.kt
+++ src/main/kotlin/com/ryouonritsu/inkbook_backend/config/DefaultSocketHandler.kt
@@ -1,19 +1,26 @@
 package com.ryouonritsu.inkbook_backend.config
 
+import com.alibaba.fastjson2.JSON
+import com.alibaba.fastjson2.JSONObject
 import com.google.gson.Gson
 import org.springframework.stereotype.Component
 import org.springframework.web.socket.*
-import java.io.IOException
 
 
 /**
+ * {
+ * token:
+ * user_id:
+ * type:
+ * id:
  *
+ * }
  * @author WuKunchao
  */
 @Component
 class DefaultHandler : WebSocketHandler {
 
-    var users = mutableListOf<WebSocketSession>()
+    var users = HashMap<WebSocketSession, Int>()
 
     /**
      * 建立连接
@@ -22,9 +29,9 @@ class DefaultHandler : WebSocketHandler {
      */
     @Throws(Exception::class)
     override fun afterConnectionEstablished(session: WebSocketSession) {
-        System.out.println("成功建立连接");
-        session.sendMessage(TextMessage("成功建立socket连接"));
-        users.add(session)
+        println("成功建立连接");
+        // session.sendMessage(TextMessage("成功建立socket连接"));
+        // users.add(session)
         // session.sendMessage(TextMessage("发送一波消息"));
         // System.out.println("关闭。。。。。。。。。。。");
         // session.close(); // 发送完就关闭
@@ -39,19 +46,38 @@ class DefaultHandler : WebSocketHandler {
      */
     @Throws(Exception::class)
     override fun handleMessage(session: WebSocketSession, message: WebSocketMessage<*>) {
-        System.out.println("0000" + message.payload)
-        val message1 = TextMessage("server:$message")
-        try {
-            session.sendMessage(message1)
-        } catch (e: IOException) {
-            e.printStackTrace()
+        val msg = message.payload.toString()
+        if ("token" in msg) {
+            val gson = Gson()
+            val res = gson.fromJson(msg, Info::class.java)
+            val type = res.type
+            var id = res.id.toInt()
+            if (type == "axure") {
+                id += 0x3f3f3f3f
+            }
+            users.put(session, id)
+            println("用户喜加一：" + res.user_id)
+            println("目前在线用户数：" + users.size)
+            return
         }
+
         val gson = Gson()
-        println(message)
-        val res = gson.fromJson(message.payload.toString(), Data::class.java)
-        println(res)
-        users.forEach {
-            it.sendMessage(TextMessage(res.msg))
+        val res = gson.fromJson(msg, Data::class.java)
+        if (res.type == "doc") {
+            println("开始同步文档！")
+            users.forEach {
+                if (res.id.toInt() == it.value && session != it.key) {
+                    it.key.sendMessage(TextMessage(gson.toJson(res)))
+                }
+            }
+        } else if (res.type == "axure") {
+            println("开始同步原型！")
+            val axureId = res.id.toInt() + 0x3f3f3f3f
+            users.forEach {
+                if (axureId == it.value && session != it.key) {
+                    it.key.sendMessage(TextMessage(gson.toJson(res)))
+                }
+            }
         }
     }
 
@@ -66,7 +92,7 @@ class DefaultHandler : WebSocketHandler {
         if (session.isOpen()) {
             session.close();
         }
-        System.out.println("连接出错");
+        println("连接出错");
     }
 
     /**
@@ -77,7 +103,7 @@ class DefaultHandler : WebSocketHandler {
      */
     @Throws(Exception::class)
     override fun afterConnectionClosed(session: WebSocketSession, closeStatus: CloseStatus) {
-        System.out.println("连接已关闭：" + closeStatus);
+        println("连接已关闭：" + closeStatus);
         users.remove(session)
     }
 
@@ -86,6 +112,21 @@ class DefaultHandler : WebSocketHandler {
     }
 }
 
+class Info {
+    lateinit var token: String
+    lateinit var user_id: String
+    lateinit var type: String // axure of doc
+    lateinit var id: String // if axure: id += 0x3f3f3f3f
+}
+
 class Data {
-    lateinit var msg: String
+    lateinit var type: String // axure of doc
+    lateinit var id: String // if axure: id += 0x3f3f3f3f
+    // 原型
+    lateinit var config: String
+    lateinit var items: String
+    // lateinit var config_id: String
+    // 文档
+    lateinit var name: String
+    lateinit var content: String
 }
