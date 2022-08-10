55f779d30232de9fa610a8e889acb74b20938e4e
RyouonRitsu <zhou.hongxi0903@qq.com>
2022-08-10 18:31:48 +0800

    删除目录和文件夹

--- src/main/kotlin/com/ryouonritsu/inkbook_backend/repository/DocumentationRepository.kt
+++ src/main/kotlin/com/ryouonritsu/inkbook_backend/repository/DocumentationRepository.kt
@@ -21,8 +21,16 @@ interface DocumentationRepository : JpaRepository<Documentation, Long> {
     @Query("SELECT d FROM Documentation d WHERE d.team.teamId = ?2 AND (d.dname LIKE %?1% OR d.ddescription LIKE %?1% OR d.dcontent LIKE %?1%) ORDER BY d.lastedittime DESC")
     fun findByKeywordAndTeamId(keyword: String, teamId: Int): List<Documentation>
 
+    @Query("SELECT d FROM Documentation d WHERE d.project.project_id = ?2 AND (d.dname LIKE %?1% OR d.ddescription LIKE %?1% OR d.dcontent LIKE %?1%) ORDER BY d.lastedittime DESC")
+    fun findByKeywordAndProjectId(keyword: String, projectId: Int): List<Documentation>
+
     @Transactional
     @Modifying
     @Query("DELETE FROM Documentation d WHERE d.did = ?1")
     override fun deleteById(id: Long)
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Documentation d WHERE d = ?1")
+    override fun delete(entity: Documentation)
 }
