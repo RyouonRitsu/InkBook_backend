8cbab5d8edf3627abda5b10f355182b7b6d0be6a
kude77 <412724618@qq.com>
2022-08-11 04:06:26 +0800

    文档同步完成

--- src/main/kotlin/com/ryouonritsu/inkbook_backend/config/DefaultSocketHandler.kt
+++ src/main/kotlin/com/ryouonritsu/inkbook_backend/config/DefaultSocketHandler.kt
@@ -45,6 +45,7 @@ class DefaultHandler : WebSocketHandler {
     @Throws(Exception::class)
     override fun handleMessage(session: WebSocketSession, message: WebSocketMessage<*>) {
         val msg = message.payload.toString()
+        // println(msg)
         if ("token" in msg) {
             val gson = Gson()
             val res = gson.fromJson(msg, Info::class.java)
@@ -52,39 +53,42 @@ class DefaultHandler : WebSocketHandler {
             var id = res.id.toInt()
             if (type == "axure") {
                 id += 0x3f3f3f3f
+                val syn = Data()
+                syn.op = "send_syn"
+                var otherEditer = false
+                try {
+                    users.forEach {
+                        if (id == it.value && session != it.key) { // 找到一个其他正在编辑当前原型的来同步所有人
+                            it.key.sendMessage(TextMessage(gson.toJson(syn)))
+                            throw Error()
+                        }
+                    }
+                } catch (e: Error) {
+                    otherEditer = true
+                    // println("强制同步所有人")
+                }
+                if (!otherEditer) {
+                    // println("初始化原型：" + res.id)
+                    syn.op = "origin"
+                    session.sendMessage(TextMessage(gson.toJson(syn)))
+                }
             }
             users.put(session, id)
-            val syn = Data()
-            syn.op = "send_syn"
-            var otherEditer = false
-            try {
-                users.forEach {
-                    if (id == it.value && session != it.key) { // 找到一个其他正在编辑当前原型的来同步所有人
-                        it.key.sendMessage(TextMessage(gson.toJson(syn)))
-                        throw Error()
-                    }
-                }
-            } catch (e: Error) {
-                otherEditer = true
-                // println("强制同步所有人")
-            }
-            if (!otherEditer) {
-                // println("初始化原型：" + res.id)
-                syn.op = "origin"
-                session.sendMessage(TextMessage(gson.toJson(syn)))
-            }
             // println("用户喜加一：" + res.user_id)
             println(res.user_id + " 加入了，目前在线用户数：" + users.size)
             return
         }
         // println(msg)
+        if (msg.isNullOrBlank()) { // 空消息保持在线
+            return
+        }
 
         val gson = Gson()
         val res = gson.fromJson(msg, Data::class.java)
         if (res.type == "doc") {
-            println("开始同步文档！")
             users.forEach {
                 if (res.id.toInt() == it.value && session != it.key) {
+                    // println("开始同步文档！")
                     it.key.sendMessage(TextMessage(gson.toJson(res)))
                 }
             }
